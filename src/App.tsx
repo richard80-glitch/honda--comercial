a44ef62404908cd6a8910456e5e04441de88d25a
richard80-glitch <lazalarichard80@gmail.com>
2026-07-29 14:31:47 -0400

    Actualizacion del proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-     <h1>Hondacomercial</h1>
+      <h1>Hondacomercial</h1>
+       <h1>con dios todo es posible</h1>
     </div>
   )
 }
